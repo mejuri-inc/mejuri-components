ffa9df379d8c0e8f93e55eb052828fab8370340c
Joaquin Enriquez <joaenriquez@Joaquins-MacBook-Pro.local>
2020-05-12 14:21:55 -0300

    Updates

---
+++ precache-manifest.15a4afa043389aa739e8a1e68fbe475b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcfc9969434cb695d3d995122ad99f65",
+    "url": "/mejuri-components/index.html"
+  },
+  {
+    "revision": "2107f409bdd0077d1c3e",
+    "url": "/mejuri-components/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "8e467881572ad86ae432",
+    "url": "/mejuri-components/static/js/2.8383bd7d.chunk.js"
+  },
+  {
+    "revision": "73556b140791e7f1e95a61af7da7dddb",
+    "url": "/mejuri-components/static/js/2.8383bd7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2107f409bdd0077d1c3e",
+    "url": "/mejuri-components/static/js/main.21b628f7.chunk.js"
+  },
+  {
+    "revision": "d38949b85418873f1192",
+    "url": "/mejuri-components/static/js/runtime-main.b1295bc8.js"
+  },
+  {
+    "revision": "4bb8090f3473704cb4a99efa70faf741",
+    "url": "/mejuri-components/static/media/arrow-long-left~mdAzpNRE.4bb8090f.svg"
+  },
+  {
+    "revision": "5937016838de404f4c082f6d536fb54d",
+    "url": "/mejuri-components/static/media/bag~gcUyBZpP.59370168.svg"
+  },
+  {
+    "revision": "c601aa6ac6e05bdbd7d1058ea60054d1",
+    "url": "/mejuri-components/static/media/chevron~FicxRuTq.c601aa6a.svg"
+  },
+  {
+    "revision": "f9684b74389a8fd1d05af39af0328fdb",
+    "url": "/mejuri-components/static/media/close-icon~JDMNDJqk.f9684b74.svg"
+  },
+  {
+    "revision": "251435a95d0202535b2bad8a95aef7a4",
+    "url": "/mejuri-components/static/media/hamburguer~bggXhFWQ.251435a9.svg"
+  },
+  {
+    "revision": "e5af59a85061ccc447b67938c8b40589",
+    "url": "/mejuri-components/static/media/logo~XjKPrCrd.e5af59a8.svg"
+  },
+  {
+    "revision": "ac834ea499f33439b1a1a1a5befe447d",
+    "url": "/mejuri-components/static/media/loupe~bnNZffbQ.ac834ea4.svg"
+  }
+]);
