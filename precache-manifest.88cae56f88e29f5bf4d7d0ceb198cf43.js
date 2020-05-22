6438cbbcd100d7049f1b81d0e03db74110b57364
FacuPerri <facuperri.utn@gmail.com>
2020-05-22 11:59:29 -0300

    Updates

---
+++ precache-manifest.88cae56f88e29f5bf4d7d0ceb198cf43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f8cbf572632a2ea16cd7228b9dea7f",
+    "url": "/mejuri-components/index.html"
+  },
+  {
+    "revision": "71459c04f7a55bd4d71d",
+    "url": "/mejuri-components/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "d79a4402f8f6707fefc2",
+    "url": "/mejuri-components/static/js/2.55ea489b.chunk.js"
+  },
+  {
+    "revision": "73556b140791e7f1e95a61af7da7dddb",
+    "url": "/mejuri-components/static/js/2.55ea489b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71459c04f7a55bd4d71d",
+    "url": "/mejuri-components/static/js/main.86c4f55b.chunk.js"
+  },
+  {
+    "revision": "d38949b85418873f1192",
+    "url": "/mejuri-components/static/js/runtime-main.b1295bc8.js"
+  }
+]);
