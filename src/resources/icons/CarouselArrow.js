47e8b3307e5d1c996409653a673c0c3cfbfbeb6c
patopato <patopato@la-polentosa.local>
2020-06-01 19:05:19 -0300

    TEC-2869 Add carousel arrow icon

---
+++ src/resources/icons/CarouselArrow.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Icon = ({ className }) => {
+  return (
+    <svg
+      className={className}
+      version='1.0'
+      xmlns='http://www.w3.org/2000/svg'
+      width='222.667'
+      height='158.667'
+      viewBox='0 0 167 119'
+    >
+      <path d='M118.1 9.6l-2.1 2.6 3.9 4.7c2.1 2.5 10.5 12.2 18.6 21.6l14.6 17-76.5.3L0 56v8h76.5c42.1 0 76.5.3 76.5.6s-1.5 2.3-3.2 4.3c-1.8 2.1-10.2 11.7-18.7 21.5l-15.3 17.8 2.7 2.8 2.8 2.9 22.8-26.7c12.6-14.7 22.8-27 22.6-27.4-.5-1.4-45-52.8-45.8-52.8-.4 0-1.7 1.2-2.8 2.6z' />
+    </svg>
+  )
+}
+
+export default Icon
