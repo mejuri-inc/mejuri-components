c5b9a27716af9a2c80d3323416a3948fb8eb6e57
FacuPerri <facuperri.utn@gmail.com>
2020-06-08 15:34:53 -0300

    TEC-3044 Missing Rebase File

--- src/components/MobileMenu/components/MobileMenuSignOut/styled.js
+++ src/components/MobileMenu/components/MobileMenuSignOut/styled.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
+import colors from 'styles/colors'
 
 export const SignOut = styled.div`
-  border-top: #fafafa;
+  border-top: 1px solid ${colors.lightGray3};
   font-weight: 500;
   font-size: 15px;
   margin-top: 30px;
