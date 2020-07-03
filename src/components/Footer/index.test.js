9ad8fc57f6dab238f1052f7160162384644fbb0f
FacuPerri <facuperri.utn@gmail.com>
2020-07-03 11:55:11 -0300

    TEC-3145 footer test

---
+++ src/components/Footer/index.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import Footer from './index'
+
+describe('Footer', () => {
+  it('does not break if no props', () => {
+    const { container } = render(<Footer />)
+    expect(container).toBeNull
+  })
+})
