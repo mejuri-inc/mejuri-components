16973a46d800a516fc5ab2e40fbe14ffd4748eec
Patricio Gabriel Maseda <patricio.mase@gmail.com>
2020-07-10 17:30:46 -0300

    [NO-TICKET] Fix header height

--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -3,6 +3,10 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
   height: 60px;
   width: 100%;
+
+  @media screen and (min-width: 990px) {
+    height: 80px;
+  }
 `
 Wrapper.displayName = 'HeaderWrapper'
 
