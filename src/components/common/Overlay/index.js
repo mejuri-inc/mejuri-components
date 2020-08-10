2186c3704365b35b9ea2e1f7c5a368208e8b4a5c
Patricio Gabriel Maseda <patricio.mase@gmail.com>
2020-08-10 12:50:01 -0300

    [TEC-3493] Fix overlay ref

--- src/components/common/Overlay/index.js
+++ src/components/common/Overlay/index.js
@@ -19,9 +19,9 @@ export default function Overlay({ innerRef }) {
   const ownRef = useRef(null)
 
   useEffect(() => {
-    disableBodyScroll(innerRef.current || ownRef.current)
+    disableBodyScroll((innerRef && innerRef.current) || ownRef.current)
     return function cleanBodyScroll() {
-      enableBodyScroll(innerRef.current || ownRef.current)
+      enableBodyScroll((innerRef && innerRef.current) || ownRef.current)
     }
   })
 
