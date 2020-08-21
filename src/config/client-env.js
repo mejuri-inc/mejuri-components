f1fbc8577896ab99dd210bcc28f74cf6e1a8687b
Luis Larghi <windinglair@gmail.com>
2020-08-21 19:38:16 -0300

    [TEC-3514] Export global state context provider

---
+++ src/config/client-env.js
@@ -0,0 +1,18 @@
+module.exports = {
+  CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+  CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+  CONTENTFUL_PREVIEW_ACCESS_TOKEN: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
+  CONTENTFUL_ENVIRONMENT: process.env.CONTENTFUL_ENVIRONMENT,
+  MEJURI_API_HOST: process.env.REACT_APP_MEJURI_API_HOST,
+  ASSETS_HOST: process.env.ASSETS_HOST,
+  RECAPTCHA_KEY: process.env.RECAPTCHA_KEY,
+  ALGOLIA_APP: process.env.ALGOLIA_APP,
+  ALGOLIA_KEY: process.env.ALGOLIA_KEY,
+  ALGOLIA_INDEX: process.env.ALGOLIA_INDEX,
+  SEGMENT_KEY: process.env.SEGMENT_KEY,
+  ONE_TRUST_KEY: process.env.ONE_TRUST_KEY,
+
+  contentfulApiHost: 'cdn.contentful.com',
+  contentfulPreviewApiHost: 'preview.contentful.com',
+  cloudinaryCloudName: 'mejuri-com'
+}
