1ca1b0fb385fde6f23882b9876879a947f40720a
Luis Larghi <windinglair@gmail.com>
2020-08-24 13:23:15 -0300

    Move session and api logic to Provider folder + remove client-envs file

--- src/services/api/order/index.js
+++
@@ -1,7 +0,0 @@
-import fetchApi from '../connector'
-import get from 'lodash.get'
-
-export const fetchOrder = (state, number) => {
-  const orderNumber = number || get(state, 'session.order.number')
-  return fetchApi(state, `/shop/api/orders/${orderNumber}`)
-}
--- src/services/session/apiAuth/index.js
+++
@@ -1,5 +0,0 @@
-import fetchSession from '../connector'
-
-export default function (state) {
-  return fetchSession(state, '/session_current')
-}
