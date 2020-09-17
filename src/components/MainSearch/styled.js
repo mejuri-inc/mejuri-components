403e81e9c1022ae30d45d1b20ebede6c0cee343f
Luis Larghi <windinglair@gmail.com>
2020-09-17 13:15:14 -0300

    [TEC-3715] Scroll up when open Search on mobile

--- src/components/MainSearch/styled.js
+++ src/components/MainSearch/styled.js
@@ -35,9 +35,9 @@ export const Wrapper = styled.section`
   z-index: 1;
   max-height: 100vh;
   padding: 20px;
+  position: absolute;
   @media screen and (min-width: 768px) {
     padding-top: 30px;
-    position: absolute;
   }
 `
 
