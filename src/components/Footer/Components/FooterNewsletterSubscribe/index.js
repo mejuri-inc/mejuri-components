72df9d93395fb5a47265aaa46766ea01f0a6b7c2
Luis Larghi <windinglair@gmail.com>
2020-10-07 18:09:03 -0300

    [TEC-3948] Upgrade version to 1.4.0

--- src/components/Footer/Components/FooterNewsletterSubscribe/index.js
+++ src/components/Footer/Components/FooterNewsletterSubscribe/index.js
@@ -1,61 +1,51 @@
-import React, { PureComponent } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { Wrapper, Title, Subtitle, WelcomeMessage } from './styled'
 import { FormattedMessage } from 'react-intl'
 import Input from 'components/Input'
 import { ValidationHelper } from 'helpers/validation'
 
-export default class FooterNewsletterSubscribe extends PureComponent {
-  static propTypes = {
-    track: PropTypes.func
-  }
-
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      isSubmitted: false
-    }
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  handleSubmit = (email) => {
-    const { track } = this.props
+const FooterNewsletterSubscribe = ({ track }) => {
+  const [isSubmitted, setIsSubmitted] = useState(false)
+  const handleSubmit = (email) => {
     track('subscribeNewsletter', {
       email,
       source: 'Bottom form'
     })
-    this.setState({ isSubmitted: true })
+    setIsSubmitted(true)
   }
 
-  render() {
-    const { isSubmitted } = this.state
-    return (
-      <Wrapper>
-        {!isSubmitted && (
-          <div>
-            <Title>
-              <FormattedMessage id='footer.newsletterSubscribe.title' />
-            </Title>
-            <Subtitle>
-              <FormattedMessage id='footer.newsletterSubscribe.subtitle' />
-            </Subtitle>
-            <Input
-              submit={this.handleSubmit}
-              validationErrorId='form.error.string.email'
-              placeholderId='form.field.newsletterSubscribe'
-              emptyValueErrorId='form.error.mixed.required'
-              validateValue={ValidationHelper.validEmail}
-              data-h='footer-newsletter-input'
-            />
-          </div>
-        )}
-        {isSubmitted && (
-          <WelcomeMessage>
-            <FormattedMessage id='footer.newsletterSubscribe.welcome' />
-          </WelcomeMessage>
-        )}
-      </Wrapper>
-    )
-  }
+  return (
+    <Wrapper>
+      {!isSubmitted && (
+        <div>
+          <Title>
+            <FormattedMessage id='footer.newsletterSubscribe.title' />
+          </Title>
+          <Subtitle>
+            <FormattedMessage id='footer.newsletterSubscribe.subtitle' />
+          </Subtitle>
+          <Input
+            submit={handleSubmit}
+            validationErrorId='form.error.string.email'
+            placeholderId='form.field.newsletterSubscribe'
+            emptyValueErrorId='form.error.mixed.required'
+            validateValue={ValidationHelper.validEmail}
+            data-h='footer-newsletter-input'
+          />
+        </div>
+      )}
+      {isSubmitted && (
+        <WelcomeMessage>
+          <FormattedMessage id='footer.newsletterSubscribe.welcome' />
+        </WelcomeMessage>
+      )}
+    </Wrapper>
+  )
 }
+
+FooterNewsletterSubscribe.propTypes = {
+  track: PropTypes.func
+}
+
+export default FooterNewsletterSubscribe
