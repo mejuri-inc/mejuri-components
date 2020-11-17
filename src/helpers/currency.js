f69e7de2fa2dee4bdbb44633bd1a3672349260d0
Patricio Gabriel Maseda <patricio.mase@gmail.com>
2020-11-17 18:38:46 -0300

    [TEC-4347] Fix decimals

--- src/helpers/currency.js
+++ src/helpers/currency.js
@@ -7,6 +7,8 @@ export const currencies = {
   GBP: '£'
 }
 
+const isInt = (n) => n % 1 === 0
+
 export const toCurrency = (amount, currency = 'USD', whenZero) => {
   const prefix = amount < 0 ? '-' : ''
   const symbol = currencies[currency] || ''
@@ -14,7 +16,12 @@ export const toCurrency = (amount, currency = 'USD', whenZero) => {
 
   if (formattedAmount === 0 && whenZero !== undefined) return whenZero
 
-  return `${prefix}${symbol}${formattedAmount}`
+  const displayAmount =
+    formattedAmount === 0 || isInt(formattedAmount)
+      ? formattedAmount
+      : formattedAmount.toFixed(2)
+
+  return `${prefix}${symbol}${displayAmount}`
 }
 
 export const toCurrencyWithCode = (amount, currency = 'USD', whenZero) =>
